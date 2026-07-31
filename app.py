f390faf835626949ed208e2f82d1419cb99f9ca5
ENPGUY <152174869+ENPGUY@users.noreply.github.com>
2026-07-31 21:28:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pdfplumber
-from src.RAG import ContractAnalysisRAG
-from src.RAG import ContractLLM
+from SRC.ContractAnalysisRAG import ContractAnalysisRAG
 
 
 # --------------------------------------------------
